59bf66544ccea7ca7d707c17aaec049bc4c262db
shangxinxiaoshuaige <1270046584@qq.com>
2020-06-11 13:09:45 +0800

    first commit

---
+++ zelinf5/toutiao/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <div class="nav-item">123</div>
+      <div class="nav-item">123</div>
+      <div class="nav-item">123</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style  lang='less' scoped>
+#app {
+  .nav {
+    .nav-item {
+      color: red;
+    }
+  }
+}
+</style>
