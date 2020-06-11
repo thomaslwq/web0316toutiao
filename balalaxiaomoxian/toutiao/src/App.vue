2369038da20a52f61f174890fa6466ff839ff420
zav1n <1114370611@qq.com>
2020-06-11 15:34:56 +0800

    toutiao

---
+++ balalaxiaomoxian/toutiao/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ balalaxiaomoxian/toutiao/src/components/Index.vue
@@ -0,0 +1,28 @@
+<template>
+<div class='header'>
+    <Header></Header>
+</div>
+</template>
+
+<script>
+import Header from "../components/header/Header"
+export default {
+components: {
+    Header,
+},
+data() {
+   return {
+
+   };
+},
+
+methods: {
+
+},
+
+
+}
+</script>
+<style  scoped>
+
+</style>
