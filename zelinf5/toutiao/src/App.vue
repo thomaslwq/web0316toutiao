887a783d415f2a50e979016de77550d558f71815
shangxinxiaoshuaige <1270046584@qq.com>
2020-06-11 16:17:49 +0800

    初始化

---
+++ zelinf5/toutiao/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <div class="nav-item">123</div>
+      <div class="nav-item">123</div>
+      <div class="nav-item">123</div>
+      <div class="nav-item">123</div>
+      <div class="nav-item">123</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang='less' scope>
+  #app {
+    .nav {
+      .nav-item {
+        color:red;
+    }
+  }
+}
+</style>
