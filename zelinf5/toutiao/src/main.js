83a5eb32a09ac4599bde18cf30275e1391b1ed69
shangxinxiaoshuaige <1270046584@qq.com>
2020-06-12 22:33:56 +0800

    第二天

--- zelinf5/toutiao/src/main.js
+++ zelinf5/toutiao/src/main.js
@@ -1,6 +1,10 @@
 // 1. 引入Vue
 import Vue from 'vue'
 import App from './App.vue'
+// b.1 引入axios 请求库
+import axios from "./common/axios"
+// b。2挂载到 Vue的原型上
+Vue.prototype.$axios = axios;
 // 2. 引入路由组件
 import VueRouter from "vue-router"
 // 2.2使用vuerouter
